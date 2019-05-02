c4d8bfb0d555a9562de820d1024aeb3df7b236a2
Nick Lee <leexnick@gmail.com>
2019-05-02 11:31:58 -0700

    modularized cors-whitelist

--- src/app.js
+++ src/app.js
@@ -6,20 +6,144 @@ const morgan = require('morgan');
 const cors = require('cors');
 const corsOptions = require('./cors-whitelist');
 const helmet = require('helmet');
+const uuid = require('uuid/v4');
 const { NODE_ENV } = require('./config');
 
 const app = express();
+app.use(express.json());
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'dev';
 
 app.use(morgan(morganOption));
-app.use(cors(corsOptions));
+
+app.use( cors( {origin: corsOptions} ) );
 app.use(helmet());
 
+const users = [
+  {
+    'id': '3c8da4d5-1597-46e7-baa1-e402aed70d80',
+    'username': 'sallyStudent',
+    'password': 'c00d1ng1sc00l',
+    'favoriteClub': 'Cache Valley Stone Society',
+    'newsLetter': 'true'
+  },
+  {
+    'id': 'ce20079c-2326-4f17-8ac4-f617bfd28b7f',
+    'username': 'johnBlocton',
+    'password': 'veryg00dpassw0rd',
+    'favoriteClub': 'Salt City Curling Club',
+    'newsLetter': 'false'
+  }
+];
+
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res
+    .send('A GET Request');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res
+    .send('POST request received');
+});
+
+app.post('/register', (req, res) => {
+  // get the data
+  const { username, password, favoriteClub, newsLetter = false } = req.body;
+
+  // All are required, check if they were sent
+  if (!username) {
+    return res
+      .status(400)
+      .send('Username required');
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .send('Password required');
+  }
+
+  if (!favoriteClub) {
+    return res
+      .status(400)
+      .send('favorite Club required');
+  }
+
+  // make sure username is correctly formatted.
+  if (username.length < 6 || username.length > 20) {
+    return res
+      .status(400)
+      .send('Username must be between 6 and 20 characters');
+  }
+
+  // password length
+  if (password.length < 8 || password.length > 36) {
+    return res
+      .status(400)
+      .send('Password must be between 8 and 36 characters');
+  }
+
+  // password contains digit, using a regex here
+  if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
+    return res
+      .status(400)
+      .send('Password must be contain at least one digit');
+  }
+
+  const clubs = [
+    'Cache Valley Stone Society',
+    'Ogden Curling Club',
+    'Park City Curling Club',
+    'Salt City Curling Club',
+    'Utah Olympic Oval Curling Club'
+  ];
+
+  // make sure the club is valid
+  if (!clubs.includes(favoriteClub)) {
+    return res
+      .status(400)
+      .send('Not a valid club');
+  }
+
+  const id = uuid();
+  const newUser = {
+    id,
+    username,
+    password,
+    favoriteClub,
+    newsLetter
+  };
+
+  users.push(newUser);
+
+  // at this point all validation passed
+  res.send('All validation passed');
+});
+
+app.get('/user', (req, res) => {
+  res
+    .json(users);
+});
+
+app.delete('/user/:userId', (req, res) => {
+  const { userId } = req.params;
+  const index = users.findIndex(u => u.id === userId);
+
+  // make sure we actually find a user with that id
+  if (index === -1) {
+    return res
+      .status(404)
+      .send('User not found');
+  }
+
+  users.splice(index, 1);
+
+  res
+    .status(204)
+    .end();
 });
 
 app.use(function errorHandler(error, req, res, next) {
--- src/cors-whitelist.js
+++ src/cors-whitelist.js
@@ -1,16 +1,13 @@
 'use strict';
 
-import { builtinModules } from "module";
-
 const whitelist = ['http://localhost:3000', 'http://my-project.com'];
-const options = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
+
+const originGenerator = function (origin, callback) {
+  if (whitelist.indexOf(origin) !== -1 || !origin) {
+    callback(null, true);
+  } else {
+    callback(new Error('Not allowed by CORS'));
   }
 };
 
-module.exports = options;
+module.exports = originGenerator;
